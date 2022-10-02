25c51a1d9c56fc21a8de00701dbb2d6521699e1a
Biraj Pudasaini <biraj2019pudasaini@gmail.com>
2022-10-02 14:10:37 +0545

    main file work done

--- maincalculator.py
+++ maincalculator.py
@@ -1,2 +1,16 @@
-#work done from biraj pudasaini2
-#work done from biraj pudasaini 1
+
+import add_work
+
+import sub_work
+
+import mul_work
+
+import div_work
+a=300
+b=10
+total_sum =add_work.add(a,b)
+total_diff=sub_work.sub(a,b)
+total_product=mul_work.mul(a,b)
+total_quotient=div_work.div(a,b)
+
+print("Total sum is {total_sum}. Total diff is {total_diff}.Total pro is {total_product}")
