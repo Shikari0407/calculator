5f1ef59e40dfa413f65b8d552c2b4b091c07d430
Biraj Pudasaini <biraj2019pudasaini@gmail.com>
2022-10-06 17:44:27 +0545

    main file modified

--- maincalculator.py
+++ maincalculator.py
@@ -6,11 +6,13 @@ import sub_work
 import mul_work
 
 import div_work
+import mod_work
 a=300
 b=10
 total_sum =add_work.add(a,b)
 total_diff=sub_work.sub(a,b)
 total_product=mul_work.mul(a,b)
 total_quotient=div_work.div(a,b)
+total_remainder=mod_work.mod(a,b)
 
-print(f"Total sum is {total_sum}. Total diff is {total_diff}.Total pro is {total_product}")
+print(f"Total sum is {total_sum}. Total diff is {total_diff}.Total pro is {total_product}.total quotient is {total_quotient}.total remainder is {total_remainder}")
